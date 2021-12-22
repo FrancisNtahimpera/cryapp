4f2ee3a5305f82ad823ab17018bf8f9d75a3a15e
francisntahimpera <francisntahimpera@gmail.com>
2021-12-22 01:12:22 +0100

    connection API Currency display the exchange different

--- cryptoapp/src/components/CUrrencyConverter.js
+++ cryptoapp/src/components/CUrrencyConverter.js
@@ -1,21 +1,37 @@
 import { useState } from 'react';
 import ExchangeRate from "./ExchangeRate";
-
+import axios from 'axios';
 
 const CurrencyConverter = () => {
+  const currencies = ["BTC", "ETH", "USD", "XRP", "LTC", "ADA"];
+  const [choosenPrimaryCurrency, setChoosenPrimaryCurrency] = useState("BTC");
+  const [choosenSecondaryCurrency, setchoosenSecondaryCurrency] =useState("BTC");
+  const [amount, setAmount] = useState(1);
+  const [exchangeRate, setExchangeRate] = useState(0);
+   
+  //console.log(amount);
+  
+  const convert = () => {
+     
 
-    
-        const currencies = [ 'BTC' , 'ETH' , 'USD' , 'XRP' , 'LTC' , 'ADA'  ]
-    const [choosenPrimaryCurrency , setChoosenPrimaryCurrency] = useState('BTC')
-    const [choosenSecondaryCurrency , setchoosenSecondaryCurrency] = useState('BTC')
+    const options = {
+        method: 'GET',
+        url: 'https://alpha-vantage.p.rapidapi.com/query',
+        params: {to_currency: choosenPrimaryCurrency, function: 'CURRENCY_EXCHANGE_RATE', from_currency: choosenSecondaryCurrency},
+        headers: {
+          'x-rapidapi-host': 'alpha-vantage.p.rapidapi.com',
+          'x-rapidapi-key': '2d927652d4msh0e4d92671477f29p190d09jsn7573cb1a4d74'
+        }
+      };
 
-
-    
-
-
-
-
-
+    axios.request(options).then((response) => {
+        console.log(response.data['Realtime Currency Exchange Rate']['5. Exchange Rate'])
+        setExchangeRate(response.data['Realtime Currency Exchange Rate']['5. Exchange Rate'])
+      }).catch((error) => {
+        console.error(error)
+      })
+  } 
+  console.log(exchangeRate);
   return (
     <div className="currency-converter">
       <h2>CurrencyConverter</h2>
@@ -25,16 +41,23 @@ const CurrencyConverter = () => {
             <tr>
               <td>Primary Currency </td>
               <td>
-                <input type="number" name="Currency-amount-1" value=""></input>
+                <input
+                  type="number"
+                  name="Currency-amount-1"
+                  value={amount}
+                  onChange={(e) => setAmount(e.target.value)}
+                />
               </td>
               <td>
                 <select
                   value={choosenPrimaryCurrency}
                   name="currency-option-2"
                   className="currency-options"
-                  onChange={(e) => setChoosenPrimaryCurrency(e.target.value) }
+                  onChange={(e) => setChoosenPrimaryCurrency(e.target.value)}
                 >
-                  {currencies.map((currency , _index ) =>(<option key={_index}>{currency}</option>) )}
+                  {currencies.map((currency, _index) => (
+                    <option key={_index}>{currency}</option>
+                  ))}
                 </select>
               </td>
             </tr>
@@ -44,18 +67,21 @@ const CurrencyConverter = () => {
                 <input type="number" name="Currency-amount-1" value=""></input>
               </td>
               <td>
-              <select
+                <select
                   value={choosenSecondaryCurrency}
                   name="currency-option-2"
                   className="currency-options"
-                  onChange={(e) => setchoosenSecondaryCurrency(e.target.value) }
+                  onChange={(e) => setchoosenSecondaryCurrency(e.target.value)}
                 >
-                  {currencies.map((currency , _index ) =>(<option key={_index}>{currency}</option>) )}
+                  {currencies.map((currency, _index) => (
+                    <option key={_index}>{currency}</option>
+                  ))}
                 </select>
               </td>
             </tr>
           </tbody>
         </table>
+        <button id="convert-button" onClick={convert}>CONVERT</button>
 
         <ExchangeRate />
       </div>
